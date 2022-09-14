1586e6f68adb85addd0e89113a6e138f0bf3f093
Dandy Reyes <dandyreyes@Dandys-MBP.lan>
2022-09-14 16:07:47 -0700

    Finished changing react to typescript

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -6,25 +6,6 @@ export default function MailForm() {
   const [email, setEmail] = React.useState("");
   const [bodyText, setBodyText] = React.useState("");
 
-  console.log(name, email, bodyText);
-  // const handleOnChange = (event: any) => {
-  //   const { target } = event;
-  //   const { value } = target;
-  //   const { name: namename } = target;
-  //   console.log(namename);
-  //   console.log(value);
-  //   switch (name) {
-  //     case "name":
-  //       setName(value);
-  //       break;
-  //     case "email":
-  //       setEmail(email);
-  //       break;
-  //     case "bodyText":
-  //       setBodyText(value);
-  //   }
-  // };
-
   const handleOnSubmit = (event: any) => {
     event.preventDefault();
     console.log("submitted");
@@ -54,7 +35,7 @@ export default function MailForm() {
           type="text"
           placeholder="What can I help you with?"
           name="bodyText"
-          value={bodyText || ""}
+          value={bodyText}
           onChange={(e) => setBodyText(e.target.value)}
         />
         <button type="submit">Submit</button>
