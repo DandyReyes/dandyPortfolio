736dd5c740c61b98219e7fc7b59c383e423db4a6
Dandy Reyes <dandyreyes@Dandys-MBP.lan>
2022-09-21 22:01:54 -0700

    added a uilibrary for commonly used components and made about portion responsive

---
+++ src/UILibrary/index.js
@@ -0,0 +1,12 @@
+export {
+  Container,
+  Row,
+  Col,
+  Nav,
+  Navbar,
+  Modal,
+  Carousel,
+  Button,
+  Image,
+  Toast,
+} from "react-bootstrap";
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Nav, Navbar, Container } from "src/UILibrary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   regular,
