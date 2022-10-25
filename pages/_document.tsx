099c52d090b7b12793c49e3a3dde0ba6c7de510a
Dandy Reyes <dandyreyes@Dandys-MBP.lan>
2022-10-24 19:12:25 -0700

    Changing new style of skills section

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import * as React from "react";
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
