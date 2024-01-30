a9218e6e1d090b1ec8be7238cfdb4513c8c41c3d
Dandy Reyes <dandyjreyes@gmail.com>
2024-01-30 12:55:09 -0800

    Added Google analytics 4

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,8 +6,13 @@ import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
 import Calendly from "./Calendar/Calendly";
 import { ThemeProvider } from "react-bootstrap";
+import ReactGA from "react-ga";
 
 export function MyApp() {
+  React.useEffect(() => {
+    ReactGA.initialize("G-6K1Q19BN4Y");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <ThemeProvider
       breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
