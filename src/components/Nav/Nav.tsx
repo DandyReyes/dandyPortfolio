85b91044263f973fe49cbaa426756e35d69ad7ec
Dandy Reyes <dandyjreyes@gmail.com>
2025-09-19 15:09:13 -0700

    chore: updated fortawesome latest syntax to nav.tsx

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Nav, Navbar, Container } from "src/UILibrary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  regular,
-  brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import styles from "./Nav.module.scss";
 
 export default function NavigationBar() {
@@ -15,8 +13,6 @@ export default function NavigationBar() {
     { key: 4, href: "#contact", text: "Contact" },
   ];
 
-  // Unfortunately babel macros is built in run-time so these icons can't be
-  // added into an array and looped over. This is a workaround.
   return (
     <Navbar fixed="top" expand="lg" variant="dark" bg="dark">
       <Container fluid>
@@ -30,14 +26,14 @@ export default function NavigationBar() {
             ))}
           </Nav>
           <Nav className="ms-auto" id={styles["nav-icons"]}>
-            <Nav.Link href={"https://www.github.com/dandyreyes"}>
-              <FontAwesomeIcon icon={brands("github")} />
+            <Nav.Link href="https://www.github.com/dandyreyes">
+              <FontAwesomeIcon icon={faGithub} />
             </Nav.Link>
-            <Nav.Link href={"https://www.linkedin.com/in/dandy-reyes"}>
-              <FontAwesomeIcon icon={brands("linkedin")} />
+            <Nav.Link href="https://www.linkedin.com/in/dandy-reyes">
+              <FontAwesomeIcon icon={faLinkedin} />
             </Nav.Link>
-            <Nav.Link href={"#contact"}>
-              <FontAwesomeIcon icon={regular("envelope")} />
+            <Nav.Link href="#contact">
+              <FontAwesomeIcon icon={faEnvelope} />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
